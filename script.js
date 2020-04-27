621820779718af44059f9afda854835a885e9455
goodness obi <goodnessobi@gmail.com>
2020-04-27 15:54:47 +0100

    implement fibonaci sequence for negative integers

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function fibonacciSequence(number) {
     let fibonacciArray = [0, 1];
-    if (number <= 0) {
+    if (number === 0) {
         fibonacciArray = [0];
-    } else {
-        for (i = 2; i <= number; i++) {
-            let sumArray = fibonacciArray.slice(fibonacciArray.length-2);
-            let nextNumber = sumArray[0] + sumArray[1]
-            fibonacciArray.push(nextNumber);
-        }
+    } 
+    for (i = 2; i <= number; i++) {
+        let sumArray = fibonacciArray.slice(fibonacciArray.length-2);
+        let nextNumber = sumArray[0] + sumArray[1]
+        fibonacciArray.push(nextNumber);
     }
+    
+    for (i = number + 2; i <= 0; i++) {
+        let sumArray = fibonacciArray.slice(fibonacciArray.length - 2);
+        let nextNumber = sumArray[0] - sumArray[1]
+        fibonacciArray.push(nextNumber);
+    }
+    if (number < 0) {
+        return fibonacciArray.reverse();
+    } 
     return fibonacciArray;
 }
 
-console.log(fibonacciSequence(15));
+console.log(fibonacciSequence(-15));
+
+
+
+
