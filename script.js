fc3d966da48a2685f0d8361732ceb176bfd5ad3d
goodness obi <goodnessobi@gmail.com>
2020-04-27 14:18:56 +0100

    implement fibonacci sequence

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-// function fibonacciSequence(number) {
-
-// }
-
-// fibonacciSequence(15);
-
-
-
-
-let fibonacciArray = [0, 1];
-let num = 20;
-for (i = 2; i < num; i++) {
-    let sumArray = fibonacciArray.slice(fibonacciArray.length-2);
-
-    let nextNumber = sumArray[0] + sumArray[1]
-   
-    fibonacciArray.push(nextNumber);
+function fibonacciSequence(number) {
+    let fibonacciArray = [0, 1];
+    if (number <= 0) {
+        fibonacciArray = [0];
+    } else {
+        for (i = 2; i <= number; i++) {
+            let sumArray = fibonacciArray.slice(fibonacciArray.length-2);
+            let nextNumber = sumArray[0] + sumArray[1]
+            fibonacciArray.push(nextNumber);
+        }
+    }
+    return fibonacciArray;
 }
 
-console.log(fibonacciArray);
+console.log(fibonacciSequence(15));
